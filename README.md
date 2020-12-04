0179584c3d89c915710137952c04848fad1fe6c7
nimeet-remo <nimeet@remo.co>
2020-12-04 13:43:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Custom backgrounds using BodyPix and Opentok
+Filters using Google BodyPix library
+
+1. add your opentok credentials in server.js. this demo uses a fixed session-id and generates tokens for this session-id
+2. "npm install" to install dependencies
+3. "npm start" to start the sever
+4. http://localhost:8026/filters to open the webpage that uses filters
+
---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+var OpenTok = require('opentok');
+
+var apikey=process.env.APIKEY;
+var secret=process.env.SECRET;
+var sessionid=process.env.SESSION;
+var opentok = OpenTok(apikey,secret)
+
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/filter.html");
+});
+
+app.get("/filter", (request, response) => {
+  response.sendFile(__dirname + "/views/filter.html");
+});
+
+app.get("/opentokFilters/token", (request, response) => {
+  var token = opentok.generateToken(sessionid);
+  response.json({"apikey":apikey,"sessionid":sessionid,"token":token});
+});
+
+// listen for requests :)
+const listener = app.listen(3000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
