02550410798e4f30faaac592610a09897a1c0760
nimeet-remo <nimeet@remo.co>
2020-12-04 13:47:02 +0530

    Fixed port

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get("/opentokFilters/token", (request, response) => {
 });
 
 // listen for requests :)
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 8026, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
